2122389cae26f0fc3fb405507b09bc96fa46d160
Fabrizio <elsertero56789@gmail.com>
2021-06-05 18:49:08 -0500

    Agregando COORS

--- src/main/java/com/rest/proyecto/api/ClienteRest.java
+++ src/main/java/com/rest/proyecto/api/ClienteRest.java
@@ -10,10 +10,12 @@ import com.rest.proyecto.model.Cliente;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -21,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author fabrizio
  */
 @RestController
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
 @RequestMapping("clientes")
 public class ClienteRest {
     
